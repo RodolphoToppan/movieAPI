d3fa80e67b18147b9c51598be2df956ee3adab89
Rodolpho <rodolphotoppan@hotmail.com>
2023-07-12 05:07:52 -0300

    enviroment variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('express-async-errors')
+require('dotenv/config')
+
 const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 
@@ -30,5 +32,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`))
